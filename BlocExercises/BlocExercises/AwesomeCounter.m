20d1da718a0f7aa807983489c03ddc668306acfd
Roshan <rmtreks@gmail.com>
2015-04-09 14:24:17 +1000

    testThatStringWorksAscending

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,26 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSInteger lowerLimit = MIN(number, otherNumber);
+    NSInteger upperLimit = MAX(number, otherNumber);
+    
+    NSMutableString *awesomeCounterString;
+    
+    awesomeCounterString = [[NSMutableString alloc] init];
+    
+    for (NSInteger counter = lowerLimit; counter < upperLimit; counter++) {
+        [awesomeCounterString appendString:[NSMutableString stringWithFormat:@"%ld", counter]];
+        
+    };
+
+    [awesomeCounterString appendString:[NSMutableString stringWithFormat:@"%ld", upperLimit]];
+    NSLog(awesomeCounterString);
+    
+    
+    
+    
+    return awesomeCounterString;
 }
 
 @end
