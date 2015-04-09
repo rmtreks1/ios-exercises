863c0db440c3d51699e1dbfa75799182f438fcb3
Roshan <rmtreks@gmail.com>
2015-04-09 11:39:48 +1000

    testThatTwoStringsAreEqual

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,7 +12,7 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    return [string1 isEqual:string2];
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
