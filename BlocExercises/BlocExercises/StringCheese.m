0f9eb915a624a6376cc3057c2076bb4d295edc87
Roshan <rmtreks@gmail.com>
2015-04-09 10:32:40 +1000

    String exercises - favorite cheese and remove suffix

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,13 +11,43 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    /* WORK HERE
+     @code
+     NSString *fullSentence = [cheese favoriteCheeseStringWithCheese:@"cheddar"];
+     // fullSentence is "My favorite cheese is cheddar."
+     @endcode
+     */
+    NSString *favoriteCheeseHeader = @"My favorite cheese is";
+    NSString *favoriteCheeseFullStatement = [NSString stringWithFormat:@"%@ %@.", favoriteCheeseHeader, cheeseName];
+   // NSLog(favoriteCheeseFullStatement);
+    return favoriteCheeseFullStatement;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    /**
+     Removes "cheese" from the end of a string about cheese and only returns
+     the name of the cheese.
+     
+     Example usage:
+     
+     @code
+     NSString *shortName = [cheese cheeseNameWithoutCheeseSuffix:@"mozarella cheese"];
+     // shortName is "mozarella"
+     @endcode
+     
+     @param cheeseName
+     The name of the favorite cheese
+     
+     @return Returns the name of the favorite cheese without the word "cheese".
+     */
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese"];
+    NSString *cheeseNameOnly = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+    
+    // Printing out to console to test
+    NSLog(cheeseNameOnly);
+    
+    return cheeseNameOnly;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
